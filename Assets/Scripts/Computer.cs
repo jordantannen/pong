957bcb14b4ee29f01b5fdf6c4e0d5147def7adf9
Jordan Tannenbaum <jtannenbaum3@gatech.edu>
2025-08-09 22:35:10 -0700

    Add delay to computer AI

--- Assets/Scripts/Computer.cs
+++ Assets/Scripts/Computer.cs
@@ -6,18 +6,38 @@ public class Computer : MonoBehaviour
     public float maxY = 4f, minY = -4f;
     public Transform ball;
     
+    private float waitTime = 0f;
+    private float timer = 0f;
+    public float responseTimeUpper = 1f;
+    public float responseTimeLower = 0.01f;
+    float targetY;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        targetY = transform.position.y;
+        SetNextWait();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float targetY = Mathf.Clamp(ball.position.y, minY, maxY);
+        timer += Time.deltaTime;
+        if (timer >= waitTime)
+        {
+            if (ball != null)
+                targetY = Mathf.Clamp(ball.position.y, minY, maxY);
+
+            timer = 0f;
+            SetNextWait();
+        }
+        
         float newY = Mathf.MoveTowards(transform.position.y, targetY, speed * Time.deltaTime);
         transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
+    void SetNextWait()
+    {
+        waitTime = Random.Range(responseTimeLower, responseTimeUpper);
     }
 }
